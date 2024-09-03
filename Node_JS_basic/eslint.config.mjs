008239a5b459b766f2f8f3456758c3e4f4dde995
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-09-03 20:08:54 +1000

    task 0

---
+++ Node_JS_basic/eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Includes browser globals
+        ...globals.node,    // Includes Node.js globals
+      },
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
+];
